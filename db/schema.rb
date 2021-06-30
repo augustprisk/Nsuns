8bbe8339ea9397614939b10fb0a1f6cbeba75f29
dotdotdotdotdot <guss.prisk@gmail.com>
2021-06-30 20:52:10 +1200

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_065652) do
+ActiveRecord::Schema.define(version: 2021_06_30_075601) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "gyms", force: :cascade do |t|
     t.string "name"
